96bef6eb49b413abf131ea7caecebd8c9648b817
Xav <xavfranc3@gmail.com>
2022-02-15 11:11:36 +0100

    Add/Api documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,52 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="UserApi OpenApi Documentation",
+ *      description="L5 Swagger OpenApi",
+ *      @OA\Contact(
+ *          email="dev.xavier.francois@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
+ * )
+
+ *
+ * @OA\Tag(
+ *     name="User Api",
+ *     description="User API Endpoints"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @OA\Get (
+     *     path="/",
+     *     summary="Welcome page",
+     *     description="Hello",
+     *     operationId="welcome",
+     *     tags={"welcome"},
+     * @OA\Response (
+            response="200",
+     *     description="Successful",
+     *     )
+     * )
+     */
+    public function welcome()
+    {
+        return "Hello";
+    }
+
 }
