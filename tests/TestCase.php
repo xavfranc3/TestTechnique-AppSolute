5c54b8e240feda67b755173b53f5e16bcd1d94ac
Xav <xavfranc3@gmail.com>
2022-02-15 10:12:30 +0100

    Initial setup commit

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected function initDatabase() {
+        Artisan::call('migrate');
+    }
+
+    protected function resetDatabase() {
+        Artisan::call('migrate:reset');
+    }
+
+    protected function seedDatabase() {
+        Artisan::call('db:seed');
+    }
+}
